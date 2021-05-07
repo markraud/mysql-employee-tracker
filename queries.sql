48df6890d0693868dbd87ec4a73f8c1b6a665883
Mark Raudenbush <markraud@hotmail.com>
2021-05-06 23:29:53 -0600

    worked on  queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
-SELECT first_name, last_name, name, title, salary 
+// 
+SELECT employee(id), first_name, last_name, department_name, title, salary 
 	FROM employee, department, role
-    WHERE department_name = 'engineering';
+    WHERE department_name = 'Engineering';
